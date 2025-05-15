1046ce0cc0d42145899a56d58ad8230dc7b3bb91
DanielSanJP <105352895+DanielSanJP@users.noreply.github.com>
2025-05-15 16:59:44 +1200

    Typo

--- src/components/FishDetails.jsx
+++ src/components/FishDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from "../supabaseClient";
-import "../styles/FIshDetails.css";
+import "../styles/FishDetails.css";
 
 function FishDetails() {
   const { id } = useParams();
